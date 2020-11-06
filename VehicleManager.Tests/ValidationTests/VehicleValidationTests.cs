eb99a183d67fdf4f5bfddbbd62f719fa419ed46f
Albert Wagner <albertwagner95@gmail.com>
2020-11-06 12:23:17 +0100

    Finished vehicle validation

--- VehicleManager.Tests/ValidationTests/VehicleValidationTests.cs
+++ VehicleManager.Tests/ValidationTests/VehicleValidationTests.cs
@@ -22,7 +22,7 @@ namespace VehicleManager.Tests.ValidationTests
             var result = _validator.TestValidate(model);
             result.ShouldHaveValidationErrorFor(rn => rn.RegistrationNumber);
         }
-         
+
         [Fact]
         public void ShouldHaveErrorWhenApplicationUserIdIsNull()
         {
@@ -55,14 +55,74 @@ namespace VehicleManager.Tests.ValidationTests
         [Fact]
         public void ShouldHaveErrorWhenDataProductionIsGratherThenDataFirstRegistration()
         {
-            model.DateOfFirstRegistration = new DateTime(2002,11,12);
+            model.DateOfFirstRegistration = new DateTime(2002, 11, 12);
             model.YearHelper = 2004;
             var result = _validator.TestValidate(model);
             result.ShouldHaveValidationErrorFor(vin => vin.YearHelper);
 
         }
 
+        [Fact]
+        public void ShouldHaveErrorWhenOwnWeightIsGreatherThanPermisibleGrossWeight()
+        {
+            model.OwnWeight = 111;
+            model.PermissibleGrossWeight = 110;
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(ow => ow.OwnWeight);
+        }
+        [Fact]
+        public void ShouldHaveErrorWhenCapacityANdPermissibleGrossWeightWillBeHaveLessThanTwoCharacters()
+        {
+            model.OwnWeight = 1;
+            model.PermissibleGrossWeight = 1;
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(ow => ow.OwnWeight);
+            result.ShouldHaveValidationErrorFor(pgw => pgw.PermissibleGrossWeight);
+        }
 
+        [Fact]
+        public void ShouldHaveErrorWhenMillageIsLessThanZero()
+        {
+            model.Millage = -2;
+            var result = _validator.TestValidate(model);
 
+            result.ShouldHaveValidationErrorFor(mlg => mlg.Millage);
+        }
+
+        [Fact]
+        public void ShouldHaveErrorWhenEngineCapacityIsLessThanZero()
+        {
+            model.EngineCapacity = -2;
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(v => v.EngineCapacity);
+        }
+        [Fact]
+        public void ShouldHaveErrorWhenYearProductionIsToLow() // less then 1799 year
+        {
+            model.DateOfFirstRegistration = new DateTime(1001, 11, 11);
+            model.YearHelper = 1002;
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(v => v.YearHelper);
+        }
+        [Fact]
+        public void ShoudHaveErrorWhenProductionYearIsGreatherThanThisYear()
+        {
+           // model.DateOfFirstRegistration = new DateTime(DateTime.Today.Year+2, 11, 11);
+            model.YearHelper = DateTime.Today.Year + 1;
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(v => v.YearHelper);
+        }
+
+        [Fact]
+        public void ShouldHaveErrorWhenEnginePowerIsLessThanZero()
+        {
+            model.EnginePower = -2;
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(v => v.EnginePower);
+        }
     }
 }
